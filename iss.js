d9e33cd43e186d904e548ae574a3b67b7f0317ab
animward <animward@gmail.com>
2024-03-15 04:14:48 -0700

    it did not work

--- iss.js
+++ iss.js
@@ -1,24 +1,23 @@
-const request = require('request');
-
-const fetchCordsByIP = function(ip, callback) {
-  request(`http://ipwhois/${ip}`, (error, response, body) => {
-
-    if (error) {
+const fetchCoordsByIP = function(ip, callback) {
+    request(`http://ipwho.is/${ip}`, (error, response, body) => {
+  
+      if (error) {
         callback(error, null);
         return;
-        }
-
-        const parsedBody = JSON.parse(body);
-
-        if(!parsedBody.success) { 
-            const message = `Success status was ${parsedBody.success}. Server message says: ${parsedBody.message} when fetching for IP ${parsedBody.ip}`;
-            callback(Error(message), null);
-            return;
-        }
-
-        const { latitude, longitude } = parsedBody;
-        callback(null, { latitude, longitude });
+      }
+  
+      const parsedBody = JSON.parse(body);
+  
+      if (!parsedBody.success) {
+        const message = `Success status was ${parsedBody.success}. Server message says: ${parsedBody.message} when fetching for IP ${parsedBody.ip}`;
+        callback(Error(message), null);
+        return;
+      } 
+  
+      const { latitude, longitude } = parsedBody;
+  
+      callback(null, {latitude, longitude});
     });
-};
-
-module.exports = { fetchCordsByIP };
+  };
+  
+  module.exports = { fetchCoordsByIP };
