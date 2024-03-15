67861d6efa6b2697ad83039412e60fdc48bc5f07
animward <animward@gmail.com>
2024-03-15 03:50:52 -0700

    iss.js update

--- iss.js
+++ iss.js
@@ -1,17 +1,24 @@
 const request = require('request');
 
-const fetchMyIP = function(callback) {
-  request('https://api.ipify.org?format=json', (error, response, body) => {
-    if (error) return callback(error, null);
+const fetchCordsByIP = function(ip, callback) {
+  request(`http://ipwhois/${ip}`, (error, response, body) => {
 
-    if (response.statusCode !== 200) {
-      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
-      return;
-    }
+    if (error) {
+        callback(error, null);
+        return;
+        }
 
-    const ip = JSON.parse(body).ip;
-    callback(null, ip);
-  });
+        const parsedBody = JSON.parse(body);
+
+        if(!parsedBody.success) { 
+            const message = `Success status was ${parsedBody.success}. Server message says: ${parsedBody.message} when fetching for IP ${parsedBody.ip}`;
+            callback(Error(message), null);
+            return;
+        }
+
+        const { latitude, longitude } = parsedBody;
+        callback(null, { latitude, longitude });
+    });
 };
 
-module.exports = { fetchMyIP };
+module.exports = { fetchCordsByIP };
